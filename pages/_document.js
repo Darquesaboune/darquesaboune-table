bb2de007a3de2825ceffa2e30d81a1fd8fc77129
Sayaka <alice@lolita.technology>
2022-10-26 01:17:59 +0200

    the beginning

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+// pages/_document.js
+
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        <meta name="bmstable" content="header.json" />
+      </Head>
+        <body className="bg-black">
+            <Main />
+            <NextScript />
+        </body>
+    </Html>
+  )
+}
