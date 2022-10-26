c58132420e109f55e7f4cae47dd66715dff474d9
Sayaka <alice@lolita.technology>
2022-10-26 11:16:44 +0200

    added sort function

--- components/table.js
+++ components/table.js
@@ -13,6 +13,50 @@ const fetchData = (url) => {
   };
 };
 
+const sortScores = (order, info) => {
+  // mostly copypasted from original code because zzzz (with additional optimizations)
+  if (order) {
+    const orderAry = order.map((e) => e.toString());
+    info.forEach((e) => (e["_index"] = orderAry.indexOf(e["level"])));
+
+    info.sort(function (a, b) {
+      if (a["_index"] < b["_index"]) {
+        return -1;
+      } else if (a["_index"] > b["_index"]) {
+        return 1;
+      } else if (a["title"].toLowerCase() < b["title"].toLowerCase()) {
+        return -1;
+      } else if (a["title"].toLowerCase() > b["title"].toLowerCase()) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    info.forEach((e) => delete e["_index"]);
+  } else {
+    // そうでない場合はレベル順->タイトル順にソート
+    info.sort(function (a, b) {
+      var aLv = a["level"].toString();
+      var bLv = b["level"].toString();
+      if (isNaN(a["level"]) == false && isNaN(b["level"]) == false) {
+        return a["level"] - b["level"];
+      } else if (aLv < bLv) {
+        return -1;
+      } else if (aLv > bLv) {
+        return 1;
+      } else if (a["title"].toLowerCase() < b["title"].toLowerCase()) {
+        return -1;
+      } else if (a["title"].toLowerCase() > b["title"].toLowerCase()) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  }
+  return info;
+};
+
 function LevelHeader({ symbol, level, numCharts }) {
   return (
     <tr className="bg-red-900 border-b dark:bg-gray-800 dark:border-gray-700">
@@ -86,12 +130,14 @@ function TableData() {
     return <tr>Error loading data</tr>;
 
   const header = fetchHeader.data;
-  const scores = fetchScores.data;
+  let scores = fetchScores.data;
+  scores = sortScores(header.level_order, scores);
+  console.log(scores);
 
   return (
     <>
-        <LevelHeader symbol="kf" level="1" numCharts={3} />
-        <ScoreEntry />
+      <LevelHeader symbol="kf" level="1" numCharts={3} />
+      <ScoreEntry />
     </>
   );
 }
