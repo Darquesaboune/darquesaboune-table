5deb184c894959423469018b87ea2f67a09d45e4
Sayaka <alice@lolita.technology>
2022-10-26 20:34:10 +0200

    added glitch effect on title

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <div className="py-12">
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="lg:text-center">
-              <h2 className="text-lg font-semibold text-red-600">La table de difficulté</h2>
+              <h2 className="text-lg font-semibold text-red-600"><VFXSpan shader="glitch">La table de difficulté</VFXSpan></h2>
               <p className="mt-2 text-white text-xl sm:text-3xl font-bold leading-8 tracking-tight md:text-4xl sm:tracking-[1em] tracking-[0.4em]"><VFXSpan shader="pixelate">darquesaboune</VFXSpan></p>
               <p className="mt-4 max-w-2xl text-l text-gray-500 lg:mx-auto"><a href="/">Retour à la maison</a></p>
             </div>
