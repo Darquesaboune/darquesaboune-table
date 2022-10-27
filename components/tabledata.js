f3b0876a47339d389eca42c0aad72765f02bd8d7
Sayaka <alice@lolita.technology>
2022-10-27 23:30:11 +0200

    disable revalidate on focus for swr

--- components/tabledata.js
+++ components/tabledata.js
@@ -6,7 +6,9 @@ import ScoreEntry from "./scoreentry";
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const useFetchData = (url) => {
-  const { data, error, isValidating } = useSWR(url, fetcher);
+  const { data, error, isValidating } = useSWR(url, fetcher, {
+    revalidateOnFocus: false,
+  });
 
   return {
     data: data,
